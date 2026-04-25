05f52cb97d1fc4c390e8aa3bcc7bfa61f115ff9e
ashiqaa21 <ashikahaneef62@gmail.com>
2026-04-25 14:25:16 +0530

    updated

--- profile/index.php
+++ profile/index.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-header("Cache-Control: no-cache, no-store, must-revalidate");
-header("Pragma: no-cache");
-header("Expires: 0");
+
+
+
 if (!isset($_SESSION['user_id'])) {
     header('Location: ../login.php');
     exit();
