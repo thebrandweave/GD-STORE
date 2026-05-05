5cac8198c4350002b72547f39ca2d08960bdb8be
ashiqaa21 <ashikahaneef62@gmail.com>
2026-05-05 11:16:52 +0530

    updated

--- about/index.php
+++ about/index.php
@@ -7,9 +7,9 @@ require_once __DIR__ . '/../config/config.php';
 <head>
     <meta charset="UTF-8">
     <title>About - GoldenDream Shop</title>
-      <link rel="icon" type="image/png" href="assets/image/gdlogo.png">
-<link rel="shortcut icon" href="assets/image/gdlogo.ico">
-<link rel="apple-touch-icon" href="assets/image/gdlogo.png">
+      <link rel="icon" type="image/png" href="../assets/image/gdlogo.png">
+<link rel="shortcut icon" href="../assets/image/gdlogo.ico">
+<link rel="apple-touch-icon" href="../assets/image/gdlogo.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/main.css">
